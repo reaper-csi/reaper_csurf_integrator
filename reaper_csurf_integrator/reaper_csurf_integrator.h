b6b289610d1a56f4f70b0f02f8468700657230c0
Geoff Waddington <waddingtongeoff@gmail.com>
2017-04-29 18:34:52 -0300

    Initial Commit

---
+++ reaper_csurf_integrator/reaper_csurf_integrator.h
@@ -0,0 +1,13 @@
+//
+//  reaper_csurf_integrator.h
+//  reaper_csurf_integrator
+//
+//  Created by Geoff Waddington on 2017-04-29.
+//  Copyright © 2017 Geoff Waddington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface reaper_csurf_integrator : NSObject
+
+@end
---
+++ reaper_csurf_integrator/reaper_csurf_integrator.m
@@ -0,0 +1,13 @@
+//
+//  reaper_csurf_integrator.m
+//  reaper_csurf_integrator
+//
+//  Created by Geoff Waddington on 2017-04-29.
+//  Copyright © 2017 Geoff Waddington. All rights reserved.
+//
+
+#import "reaper_csurf_integrator.h"
+
+@implementation reaper_csurf_integrator
+
+@end
