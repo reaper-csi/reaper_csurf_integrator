871635f63c808993385a85f975c4e3c4efe5d414
Geoff Waddington <waddingtongeoff@gmail.com>
2017-12-07 20:32:25 -0400

    WIP primitive config UI

--- reaper_csurf_integrator/resource.h
+++ reaper_csurf_integrator/resource.h
@@ -1,26 +1,54 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by res.rc
 //
-#define IDD_SURFACEEDIT_MCU      264
-#define IDD_SURFACEEDIT_MCU1     265
+#define IDD_SURFACEEDIT_MCU             264
+#define IDD_SURFACEEDIT_MCU1            265
+
 #define IDC_CHECK1                      1000
-#define IDC_COMBO2                      1001
-#define IDC_COMBO3                      1002
 #define IDC_CHECK2                      1003
+#define IDC_CHECK3                      1004
 #define IDC_EDIT1                       1007
 #define IDC_EDIT2                       1008
+#define IDC_EDIT3                       1009
+#define IDC_EDIT4                       1010
+#define IDC_EDIT5                       1036
+#define IDC_EDIT6                       1037
+#define IDC_EDIT7                       1040
+#define IDC_EDIT8                       1041
+#define IDC_EDIT9                       1044
+#define IDC_EDIT10                      1045
+#define IDC_EDIT11                      1048
+#define IDC_EDIT12                      1049
+#define IDC_EDIT13                      1052
+#define IDC_EDIT14                      1053
+#define IDC_COMBO1                      1011
+#define IDC_COMBO2                      1001
+#define IDC_COMBO3                      1002
+#define IDC_COMBO4                      1006
+#define IDC_COMBO5                      1035
+#define IDC_COMBO6                      1038
+#define IDC_COMBO7                      1039
+#define IDC_COMBO8                      1042
+#define IDC_COMBO9                      1043
+#define IDC_COMBO10                     1046
+#define IDC_COMBO11                     1047
+#define IDC_COMBO12                     1050
+#define IDC_COMBO13                     1051
+#define IDC_COMBO14                     1054
+
 #define IDC_EDIT1_LBL                   1276
 #define IDC_EDIT2_LBL                   1277
 #define IDC_EDIT2_LBL2                  1278
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
+
