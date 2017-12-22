224b07b9f9bdce5aa85e9fa06969966bf9e35836
Geoff Waddington <waddingtongeoff@gmail.com>
2017-12-21 22:22:29 -0400

    Beginnings of config dialog

--- reaper_csurf_integrator/resource.h
+++ reaper_csurf_integrator/resource.h
@@ -4,45 +4,65 @@
 //
 #define IDD_SURFACEEDIT_MCU             264
 #define IDD_SURFACEEDIT_MCU1            265
-
 #define IDC_CHECK1                      1000
-#define IDC_CHECK2                      1003
-#define IDC_CHECK3                      1004
-#define IDC_EDIT1                       1007
-#define IDC_EDIT2                       1008
-#define IDC_EDIT3                       1009
-#define IDC_EDIT4                       1010
-#define IDC_EDIT5                       1036
-#define IDC_EDIT6                       1037
-#define IDC_EDIT7                       1040
-#define IDC_EDIT8                       1041
-#define IDC_EDIT9                       1044
-#define IDC_EDIT10                      1045
-#define IDC_EDIT11                      1048
-#define IDC_EDIT12                      1049
-#define IDC_EDIT13                      1052
-#define IDC_EDIT14                      1053
-#define IDC_COMBO1                      1011
-#define IDC_COMBO2                      1001
-#define IDC_COMBO3                      1002
+#define IDC_CHECK2                      1001
+#define IDC_CHECK3                      1002
+#define IDC_COMBO1                      1003
+#define IDC_COMBO2                      1004
+#define IDC_COMBO3                      1005
 #define IDC_COMBO4                      1006
-#define IDC_COMBO5                      1035
-#define IDC_COMBO6                      1038
-#define IDC_COMBO7                      1039
-#define IDC_COMBO8                      1042
-#define IDC_COMBO9                      1043
-#define IDC_COMBO10                     1046
-#define IDC_COMBO11                     1047
-#define IDC_COMBO12                     1050
-#define IDC_COMBO13                     1051
-#define IDC_COMBO14                     1054
-
-#define IDC_EDIT1_LBL                   1276
-#define IDC_EDIT2_LBL                   1277
-#define IDC_EDIT2_LBL2                  1278
+#define IDC_COMBO5                      1007
+#define IDC_COMBO6                      1008
+#define IDC_COMBO7                      1009
+#define IDC_COMBO8                      1010
+#define IDC_COMBO9                      1011
+#define IDC_COMBO10                     1012
+#define IDC_EDIT1                       1013
+#define IDC_EDIT2                       1014
+#define IDC_EDIT3                       1015
+#define IDC_EDIT4                       1016
+#define IDC_EDIT1_LBL                   1017
+#define IDC_EDIT5                       1017
+#define IDC_EDIT2_LBL                   1018
+#define IDC_EDIT6                       1018
+#define IDC_EDIT2_LBL2                  1019
+#define IDC_EDIT7                       1019
+#define IDC_EDIT8                       1020
+#define IDC_EDIT9                       1021
+#define IDC_EDIT10                      1022
+#define IDC_EDIT11                      1023
+#define IDC_EDIT12                      1024
+#define IDC_EDIT13                      1025
+#define IDC_EDIT14                      1026
+#define IDC_EDIT15                      1027
+#define IDC_EDIT16                      1028
+#define IDC_EDIT17                      1029
+#define IDC_EDIT18                      1030
+#define IDC_EDIT19                      1031
+#define IDC_EDIT20                      1032
+#define IDC_EDIT21                      1033
+#define IDC_COMBO11                     1034
+#define IDC_EDIT22                      1035
+#define IDC_EDIT23                      1036
+#define IDC_EDIT24                      1037
+#define IDC_EDIT25                      1038
+#define IDC_EDIT26                      1039
+#define IDC_EDIT27                      1040
+#define IDC_EDIT28                      1041
+#define IDC_EDIT29                      1042
+#define IDC_EDIT30                      1043
+#define IDC_COMBO12                     1044
+#define IDC_COMBO13                     1045
+#define IDC_COMBO14                     1046
+#define IDC_COMBO15                     1047
+#define IDC_COMBO16                     1048
+#define IDC_COMBO17                     1049
+#define IDC_COMBO18                     1050
+#define IDC_COMBO19                     1051
+#define IDC_COMBO20                     1052
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
@@ -51,4 +71,3 @@
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
-
