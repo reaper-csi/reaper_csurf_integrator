576e3cde240c22d88fe41a922334108b1cbeff43
Geoff Waddington <waddingtongeoff@gmail.com>
2017-12-28 06:48:04 -0400

    Removed unnecessary tracks reference in LogicalSurface::BuildCSurfWidgts

--- reaper_csurf_integrator/control_surface_integrator.cpp
+++ reaper_csurf_integrator/control_surface_integrator.cpp
@@ -744,26 +744,24 @@ void LogicalSurface::BuildCSurfWidgets()
             for(int i = 0; i < surface->GetNumBankableChannels(); ++i)
             {
                 string channelNumber = to_string(i + 1);;
-                string trackGUID = DAW::GetTrackGUIDAsString(numLogicalChannels++);
 
-                channel = new RealSurfaceChannel(trackGUID, surface, true);
+                channel = new RealSurfaceChannel("", surface, true);
                 surface->AddChannel(channel);
                 
-                channel->AddWidget(new PushButtonWithRelease_MidiWidget(trackGUID, surface, TrackTouched, TrackTouched + channelNumber, new MIDI_event_ex_t(0x90, 0x68 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x68 + i, 0x00)));
+                channel->AddWidget(new PushButtonWithRelease_MidiWidget("", surface, TrackTouched, TrackTouched + channelNumber, new MIDI_event_ex_t(0x90, 0x68 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x68 + i, 0x00)));
                 
-                channel->AddWidget(new EncoderCycledAction_MidiWidget(trackGUID, surface, Pan, Pan + channelNumber, new MIDI_event_ex_t(0xb0, 0x10 + i, 0x7f), new MIDI_event_ex_t(0xb0, 0x10 + i, 0x00), new MIDI_event_ex_t(0x90, 0x20 + i, 0x7f)));
+                channel->AddWidget(new EncoderCycledAction_MidiWidget("", surface, Pan, Pan + channelNumber, new MIDI_event_ex_t(0xb0, 0x10 + i, 0x7f), new MIDI_event_ex_t(0xb0, 0x10 + i, 0x00), new MIDI_event_ex_t(0x90, 0x20 + i, 0x7f)));
 
-                channel->AddWidget(new Display_MidiWidget(trackGUID, surface, TrackDisplay, TrackDisplay + channelNumber, i));
-                channel->AddWidget(new Fader14Bit_MidiWidget(trackGUID, surface, Volume, Volume + channelNumber, -72.0, 12.0, new MIDI_event_ex_t(0xe0 + i, 0x7f, 0x7f), new MIDI_event_ex_t(0xe0 + i, 0x00, 0x00)));
-                channel->AddWidget(new PushButton_MidiWidget(trackGUID, surface, RecordArm, RecordArm + channelNumber,   new MIDI_event_ex_t(0x90, 0x00 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x00 + i, 0x00)));
-                channel->AddWidget(new PushButton_MidiWidget(trackGUID, surface, Solo, Solo + channelNumber,        new MIDI_event_ex_t(0x90, 0x08 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x08 + i, 0x00)));
-                channel->AddWidget(new PushButton_MidiWidget(trackGUID, surface, Mute, Mute + channelNumber,        new MIDI_event_ex_t(0x90, 0x10 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x10 + i, 0x00)));
-                channel->AddWidget(new PushButton_MidiWidget(trackGUID, surface, Select, Select + channelNumber,      new MIDI_event_ex_t(0x90, 0x18 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x18 + i, 0x00)));
+                channel->AddWidget(new Display_MidiWidget("", surface, TrackDisplay, TrackDisplay + channelNumber, i));
+                channel->AddWidget(new Fader14Bit_MidiWidget("", surface, Volume, Volume + channelNumber, -72.0, 12.0, new MIDI_event_ex_t(0xe0 + i, 0x7f, 0x7f), new MIDI_event_ex_t(0xe0 + i, 0x00, 0x00)));
+                channel->AddWidget(new PushButton_MidiWidget("", surface, RecordArm, RecordArm + channelNumber,   new MIDI_event_ex_t(0x90, 0x00 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x00 + i, 0x00)));
+                channel->AddWidget(new PushButton_MidiWidget("", surface, Solo, Solo + channelNumber,        new MIDI_event_ex_t(0x90, 0x08 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x08 + i, 0x00)));
+                channel->AddWidget(new PushButton_MidiWidget("", surface, Mute, Mute + channelNumber,        new MIDI_event_ex_t(0x90, 0x10 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x10 + i, 0x00)));
+                channel->AddWidget(new PushButton_MidiWidget("", surface, Select, Select + channelNumber,      new MIDI_event_ex_t(0x90, 0x18 + i, 0x7f), new MIDI_event_ex_t(0x90, 0x18 + i, 0x00)));
             }
         }
     }
     
-    numLogicalChannels++;
     
     // check for immobilized channels
     // GAW TBD with lazy init for Actions this will change to EnumProjExtState, and the resulting GUIDs will be stored in Immobilized tracks list.
