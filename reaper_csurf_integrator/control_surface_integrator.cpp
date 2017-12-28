39333cc6ed3451b3af63ea306736e051d6ac2279
Geoff Waddington <waddingtongeoff@gmail.com>
2017-12-28 06:17:29 -0400

    Added Xen's code snippets, couple of tweaks

--- reaper_csurf_integrator/control_surface_integrator.cpp
+++ reaper_csurf_integrator/control_surface_integrator.cpp
@@ -248,6 +248,96 @@ void LogicalSurface::MapFX(MediaTrack* track)
     }
 }
 
+
+/*
+void SID_PCM_Source::SaveState(ProjectStateContext * ctx)
+{
+    ctx->AddLine("FILE \"%s\" %f %d %d %d %d", m_sidfn.toRawUTF8(),m_sidlen,m_sid_track, m_sid_channelmode,m_sid_sr,m_sid_render_mode);
+}
+
+int SID_PCM_Source::LoadState(const char * firstline, ProjectStateContext * ctx)
+{
+    LineParser lp;
+    char buf[2048];
+    for (;;)
+    {
+        if (ctx->GetLine(buf, sizeof(buf)))
+            break;
+        lp.parse(buf);
+        if (strcmp(lp.gettoken_str(0), "FILE") == 0)
+        {
+            m_sidfn = String(CharPointer_UTF8(lp.gettoken_str(1)));
+            m_sidlen = lp.gettoken_float(2);
+            m_sid_track = lp.gettoken_int(3);
+            m_sid_channelmode = lp.gettoken_int(4);
+            m_sid_sr = lp.gettoken_int(5);
+            m_sid_render_mode = lp.gettoken_int(6);
+        }
+        if (lp.gettoken_str(0)[0] == '>')
+        {
+            renderSID();
+            return 0;
+        }
+    }
+    return -1;
+}
+*/
+
+
+
+// C++17 version ////////////////////////////////////////////////////
+
+
+void SaveState(ProjectStateContext * ctx)
+{
+    //ctx->AddLine("FILE \"%s\" %f %d %d %d %d", m_sidfn.toRawUTF8(),m_sidlen,m_sid_track, m_sid_channelmode,m_sid_sr,m_sid_render_mode);
+}
+
+template<typename T>
+inline void setFromToken(LineParser& lp, int index, T& value)
+{
+    if constexpr (is_same<int, T>::value)
+        value = lp.gettoken_int(index);
+    else if constexpr (is_same<double, T>::value)
+        value = lp.gettoken_float(index);
+    else if constexpr (is_same<float, T>::value)
+        value = static_cast<float>(lp.gettoken_float(index));
+    else if constexpr (is_same<string, T>::value)
+        value = string(lp.gettoken_str(index));
+    //else
+        //static_assert(false, "Type not supported by LineParser");
+}
+
+template<typename... Args>
+inline void setFromTokens(LineParser& lp, Args&&... args)
+{
+    int index = 0;
+    (setFromToken(lp, ++index, args), ...);
+}
+
+int LoadState(const char * firstline, ProjectStateContext * ctx)
+{
+    LineParser lp;
+    char buf[2048];
+    for (;;)
+    {
+        if (ctx->GetLine(buf, sizeof(buf)))
+            break;
+        lp.parse(buf);
+        if (strcmp(lp.gettoken_str(0), "FILE") == 0)
+        {
+            //setFromTokens(lp, m_sidfn, m_sidlen, m_sid_track, m_sid_channelmode, m_sid_sr, m_sid_render_mode);
+        }
+        if (lp.gettoken_str(0)[0] == '>')
+        {
+            //renderSID();
+            return 0;
+        }
+    }
+    return -1;
+}
+
+
 void LogicalSurface::InitializeFXMaps()
 {
     // param  = GainReduction_dB
@@ -353,11 +443,6 @@ void LogicalSurface::InitializeFXMaps()
 
 void LogicalSurface::MapReaperLogicalControlSurfaceActions()
 {
-    //trackGUIDs_.clear();
-    
-    //for(int i = 0; i < DAW::GetNumTracks() + 1; ++i) // +1 is for ReaperMasterTrack
-        //trackGUIDs_.push_back(DAW::GetTrackGUIDAsString(i));
-        
     for(auto * surface : realSurfaces_)
     {
         string surfaceName = surface->GetName();
@@ -475,9 +560,6 @@ void LogicalSurface::BuildCSurfWidgets()
 
     for(auto & surface : realSurfaces_)
     {
-        //surface->ClearChannels();
-        //surface->ClearWidgets();
-        
         if(surface->GetName() == "Console1")
         {
             channel = new RealSurfaceChannel(ReaperLogicalControlSurface, surface, false);
