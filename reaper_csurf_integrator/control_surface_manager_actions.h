642fc47c8e8bd3b5629bd8536619102a4084a65a
Geoff Waddington <waddingtongeoff@gmail.com>
2018-01-11 06:12:11 -0400

    WIP major refactoring to incorporate SurfaceGroups

--- reaper_csurf_integrator/control_surface_manager_actions.h
+++ reaper_csurf_integrator/control_surface_manager_actions.h
@@ -16,9 +16,9 @@ class Shift_Action : public DoubleAction
 public:
     Shift_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
-        GetLogicalSurface()->SetShift(surfaceName, value);
+        GetLogicalSurface()->SetShift(groupName, surfaceName, value);
     }
 };
 
@@ -29,9 +29,9 @@ class Option_Action : public DoubleAction
 public:
     Option_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
-        GetLogicalSurface()->SetOption(surfaceName, value);
+        GetLogicalSurface()->SetOption(groupName, surfaceName, value);
     }
 };
 
@@ -42,9 +42,9 @@ class Control_Action : public DoubleAction
 public:
     Control_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
-        GetLogicalSurface()->SetControl(surfaceName, value);
+        GetLogicalSurface()->SetControl(groupName, surfaceName, value);
     }
 };
 
@@ -55,9 +55,9 @@ class Alt_Action : public DoubleAction
 public:
     Alt_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
-        GetLogicalSurface()->SetAlt(surfaceName, value);
+        GetLogicalSurface()->SetAlt(groupName, surfaceName, value);
     }
 };
 
@@ -70,22 +70,22 @@ private:
 public:
     Latched_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void SetValue(string surfaceName, double value) {}
+    virtual void SetValue(string groupName, string surfaceName, double value) {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
         if(value != 0)
         {
             lastPressed_ = clock();
-            SetValue(surfaceName, value);
-            GetLogicalSurface()->SetWidgetValue(surfaceName, widgetName, value);
+            SetValue(groupName, surfaceName, value);
+            GetLogicalSurface()->SetWidgetValue(groupName, surfaceName, widgetName, value);
         }
         else
         {
             if(clock() - lastPressed_ >  CLOCKS_PER_SEC / 4)
             {
-                SetValue(surfaceName, value);
-                GetLogicalSurface()->SetWidgetValue(surfaceName, widgetName, value);
+                SetValue(groupName, surfaceName, value);
+                GetLogicalSurface()->SetWidgetValue( groupName, surfaceName, widgetName, value);
             }
         }
     }
@@ -98,9 +98,9 @@ class LatchedZoom_Action : public Latched_Action
 public:
     LatchedZoom_Action(LogicalSurface* logicalSurface) : Latched_Action(logicalSurface)  {}
     
-    virtual void SetValue(string surfaceName, double value) override
+    virtual void SetValue(string groupName, string surfaceName, double value) override
     {
-        GetLogicalSurface()->SetZoom(surfaceName, value);
+        GetLogicalSurface()->SetZoom(groupName, surfaceName, value);
     }
 };
 
@@ -111,9 +111,9 @@ class LatchedScrub_Action : public Latched_Action
 public:
     LatchedScrub_Action(LogicalSurface* logicalSurface) : Latched_Action(logicalSurface)  {}
     
-    virtual void SetValue(string surfaceName, double value) override
+    virtual void SetValue(string groupName, string surfaceName, double value) override
     {
-        GetLogicalSurface()->SetScrub(surfaceName, value);
+        GetLogicalSurface()->SetScrub(groupName, surfaceName, value);
     }
 };
 
@@ -125,9 +125,9 @@ class SetShowFXWindows_Action : public DoubleAction
 public:
     SetShowFXWindows_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual double GetValue (string surfaceName, string widgetName) override { return GetLogicalSurface()->IsShowFXWindows(surfaceName); }
+    virtual double GetValue (string groupName, string surfaceName, string widgetName) override { return GetLogicalSurface()->IsShowFXWindows(surfaceName); }
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
         GetLogicalSurface()->SetShowOpenFXWindows(surfaceName, value);
     }
@@ -140,7 +140,7 @@ class NextMap_Action : public DoubleAction
 public:
     NextMap_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
         GetLogicalSurface()->GetManager()->NextLogicalSurface();
     }
@@ -156,7 +156,7 @@ private:
 public:
     TrackBank_Action(LogicalSurface* logicalSurface, int stride) : DoubleAction(logicalSurface), stride_(stride)   {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
         GetLogicalSurface()->AdjustTrackBank(surfaceName, stride_);
     }
@@ -169,7 +169,7 @@ class ImmobilizeSelectedTracks_Action : public DoubleAction
 public:
     ImmobilizeSelectedTracks_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
        GetLogicalSurface()->ImmobilizeSelectedTracks();
     }
@@ -182,7 +182,7 @@ class MobilizeSelectedTracks_Action : public DoubleAction
 public:
     MobilizeSelectedTracks_Action(LogicalSurface* logicalSurface) : DoubleAction(logicalSurface)  {}
     
-    virtual void Do(double value, string surfaceName, string widgetName) override
+    virtual void Do(double value, string groupName, string surfaceName, string widgetName) override
     {
         GetLogicalSurface()->MobilizeSelectedTracks();
     }
