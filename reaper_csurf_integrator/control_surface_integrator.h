48dcd43332dd2857169a86bc12410effcdf1254e
Geoff Waddington <waddingtongeoff@gmail.com>
2018-01-24 19:55:46 -0400

    WIP GUI

--- reaper_csurf_integrator/control_surface_integrator.h
+++ reaper_csurf_integrator/control_surface_integrator.h
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 #include "control_surface_integrator_Reaper.h"
 
--- reaper_csurf_integrator/resource.h
+++ reaper_csurf_integrator/resource.h
@@ -3,49 +3,26 @@
 // Used by res.rc
 //
 #define IDD_SURFACEEDIT_CSI             265
-#define IDC_CHECK1                      1000
-#define IDC_CHECK2                      1001
-#define IDC_CHECK3                      1002
-#define IDC_COMBO1                      1003
-#define IDC_COMBO2                      1004
-#define IDC_COMBO3                      1005
-#define IDC_COMBO4                      1006
-#define IDC_COMBO5                      1007
-#define IDC_COMBO6                      1008
-#define IDC_COMBO7                      1009
-#define IDC_COMBO8                      1010
-#define IDC_COMBO9                      1011
-#define IDC_COMBO10                     1012
-#define IDC_EDIT1                       1013
-#define IDC_EDIT2                       1014
-#define IDC_EDIT3                       1015
-#define IDC_EDIT4                       1016
-#define IDC_EDIT5                       1017
-#define IDC_EDIT6                       1018
-#define IDC_EDIT7                       1019
-#define IDC_EDIT8                       1020
-#define IDC_EDIT9                       1021
-#define IDC_EDIT10                      1022
-#define IDC_EDIT11                      1023
-#define IDC_EDIT12                      1024
-#define IDC_EDIT13                      1025
-#define IDC_EDIT14                      1026
-#define IDC_EDIT15                      1027
-#define IDC_EDIT16                      1028
-#define IDC_EDIT17                      1029
-#define IDC_EDIT18                      1030
-#define IDC_EDIT19                      1031
-#define IDC_EDIT20                      1032
-#define IDC_COMBO11                     1034
-#define IDC_COMBO12                     1044
-#define IDC_COMBO13                     1045
-#define IDC_COMBO14                     1046
-#define IDC_COMBO15                     1047
-#define IDC_COMBO16                     1048
-#define IDC_COMBO17                     1049
-#define IDC_COMBO18                     1050
-#define IDC_COMBO19                     1051
-#define IDC_COMBO20                     1052
+#define IDC_CHECK_MidiInMon             1000
+#define IDC_CHECK_MidiOutMon            1001
+#define IDC_CHECK_VSTParamMon           1002
+#define IDC_LIST_LogicalSurfaces        1012
+#define IDC_BUTTON_AddLogicalSurface    1013
+#define IDC_BUTTON_EditLogicalSurface   1014
+#define IDC_BUTTON_RemoveLogicalSurface2 1015
+#define IDC_BUTTON_RemoveLogicalSurface 1015
+#define IDC_LIST_SurfaceGroups          1024
+#define IDC_BUTTON_AddSurfaceGroup      1025
+#define IDC_BUTTON_EditSurfaceGroup     1026
+#define IDC_BUTTON_RemoveSurfaceGroup   1027
+#define IDC_LIST_Surfaces               1028
+#define IDC_BUTTON_AddSurface           1029
+#define IDC_BUTTON_EditSurface          1030
+#define IDC_BUTTON_RemoveSurface        1031
+#define IDC_LIST_RealSurfaces           1032
+#define IDC_BUTTON_AddRealSurface       1033
+#define IDC_BUTTON_EditRealSurface      1034
+#define IDC_BUTTON_RemoveRealSurface    1035
 
 // Next default values for new objects
 // 
@@ -53,7 +30,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
