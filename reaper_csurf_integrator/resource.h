00bdd40b868c247e20a5379cadfed2d28dde0dd8
Geoff Waddington <waddingtongeoff@gmail.com>
2018-01-25 13:18:05 -0400

    WIP GUI

--- reaper_csurf_integrator/resource.h
+++ reaper_csurf_integrator/resource.h
@@ -2,15 +2,28 @@
 // Microsoft Visual C++ generated include file.
 // Used by res.rc
 //
+#define IDD_DIALOG_SurfaceGroup         103
 #define IDD_SURFACEEDIT_CSI             265
+#define IDD_DIALOG_Surface              266
+#define IDD_DIALOG_LogicallSurface      267
+#define IDD_DIALOG_RealSurface          268
 #define IDC_CHECK_MidiInMon             1000
 #define IDC_CHECK_MidiOutMon            1001
 #define IDC_CHECK_VSTParamMon           1002
 #define IDC_LIST_LogicalSurfaces        1012
 #define IDC_BUTTON_AddLogicalSurface    1013
 #define IDC_BUTTON_EditLogicalSurface   1014
-#define IDC_BUTTON_RemoveLogicalSurface2 1015
+#define IDC_EDIT_LogicalSurfaceName     1014
 #define IDC_BUTTON_RemoveLogicalSurface 1015
+#define IDC_EDIT_LogicalSurfaceGroup    1015
+#define IDC_COMBO_RealSurface           1016
+#define IDC_COMBO_ActionTemplates       1017
+#define IDC_COMBO_FXTemplates           1018
+#define IDC_COMBO_SurfaceTemplate       1019
+#define IDC_COMBO_MidiIn                1020
+#define IDC_COMBO_MidiOut               1021
+#define IDC_EDIT_RealSurfaceName        1022
+#define IDC_EDIT_RealSurfaceNumChannels 1023
 #define IDC_LIST_SurfaceGroups          1024
 #define IDC_BUTTON_AddSurfaceGroup      1025
 #define IDC_BUTTON_EditSurfaceGroup     1026
@@ -28,9 +41,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1023
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
