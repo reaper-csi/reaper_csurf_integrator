69db1b4c84ae45c12eba8e7892c6d98fcbea7a46
Geoff Waddington <waddingtongeoff@gmail.com>
2018-06-18 06:07:42 -0300

    WIP improvements/bug fixes to MapTrackAndFXToWidgets

--- reaper_csurf_integrator/control_surface_midi_widgets.h
+++ reaper_csurf_integrator/control_surface_midi_widgets.h
@@ -9,8 +9,18 @@
 
 #include "control_surface_integrator.h"
 #include "handy_reaper_functions.h"
+#include "WDL/ptrlist.h"
 
 extern Manager* TheManager;
+
+
+int __g_projectconfig_timemode2, __g_projectconfig_timemode;
+int __g_projectconfig_measoffs;
+int __g_projectconfig_timeoffs; // double
+
+char m_mackie_lasttime[10];
+int m_mackie_lasttime_mode;
+DWORD m_mcu_timedisp_lastforce, m_mcu_meter_lastrun;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class PushButton_Midi_Widget : public Midi_Widget
@@ -45,6 +55,7 @@ public:
     PushButtonWithResendOnRelease_Midi_Widget(Midi_RealSurface* surface, string role, string name, MIDI_event_ex_t* press, MIDI_event_ex_t* release) : Midi_Widget(surface, role, name, press, release)
     {
         surface->AddWidgetToMessageMap(to_string(midiPressMessage_->midi_message[0]) + to_string(midiPressMessage_->midi_message[1]) + to_string(midiPressMessage_->midi_message[2]), this);
+        surface->AddWidgetToMessageMap(to_string(midiReleaseMessage_->midi_message[0]) + to_string(midiReleaseMessage_->midi_message[1]) + to_string(midiReleaseMessage_->midi_message[2]), this);
     }
     
     void SetValue(int displayMode, double value) override
@@ -578,6 +589,173 @@ public:
         SendMidiMessage(&midiSysExData.evt);
     }
 };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class MCU_TimeDisplay_Midi_Widget : public Midi_Widget
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+{
+public:
+    MCU_TimeDisplay_Midi_Widget(Midi_RealSurface* surface, string role, string name) : Midi_Widget(surface, role, name, new MIDI_event_ex_t(0x00, 0x00, 0x00), new MIDI_event_ex_t(0x00, 0x00, 0x00)) {}
+
+    void SetValue(int mode, double value) override
+    {
+        
+#ifndef timeGetTime
+        DWORD now = GetTickCount();
+#else
+        DWORD now = timeGetTime();
+#endif
+
+        double pp=(GetPlayState()&1) ? GetPlayPosition() : GetCursorPosition();
+        unsigned char bla[10];
+        
+        memset(bla,0,sizeof(bla));
+        
+        
+        int *tmodeptr=(int*)projectconfig_var_addr(NULL,__g_projectconfig_timemode2);
+        
+        int tmode=0;
+        
+        if (tmodeptr && (*tmodeptr)>=0) tmode = *tmodeptr;
+        else
+        {
+            tmodeptr=(int*)projectconfig_var_addr(NULL,__g_projectconfig_timemode);
+            if (tmodeptr)
+                tmode=*tmodeptr;
+        }
+        
+        if (tmode==3) // seconds
+        {
+            double *toptr = (double*)projectconfig_var_addr(NULL,__g_projectconfig_timeoffs);
+            
+            if (toptr) pp+=*toptr;
+            char buf[64];
+            sprintf(buf,"%d %02d",(int)pp, ((int)(pp*100.0))%100);
+            if (strlen(buf)>sizeof(bla)) memcpy(bla,buf+strlen(buf)-sizeof(bla),sizeof(bla));
+            else
+                memcpy(bla+sizeof(bla)-strlen(buf),buf,strlen(buf));
+            
+        }
+        else if (tmode==4) // samples
+        {
+            char buf[128];
+            format_timestr_pos(pp,buf,sizeof(buf),4);
+            if (strlen(buf)>sizeof(bla)) memcpy(bla,buf+strlen(buf)-sizeof(bla),sizeof(bla));
+            else
+                memcpy(bla+sizeof(bla)-strlen(buf),buf,strlen(buf));
+        }
+        else if (tmode==5) // frames
+        {
+            char buf[128];
+            format_timestr_pos(pp,buf,sizeof(buf),5);
+            char *p=buf;
+            char *op=buf;
+            int ccnt=0;
+            while (*p)
+            {
+                if (*p == ':')
+                {
+                    ccnt++;
+                    if (ccnt!=3)
+                    {
+                        p++;
+                        continue;
+                    }
+                    *p=' ';
+                }
+                
+                *op++=*p++;
+            }
+            *op=0;
+            if (strlen(buf)>sizeof(bla)) memcpy(bla,buf+strlen(buf)-sizeof(bla),sizeof(bla));
+            else
+                memcpy(bla+sizeof(bla)-strlen(buf),buf,strlen(buf));
+        }
+        else if (tmode>0)
+        {
+            int num_measures=0;
+            double beats=TimeMap2_timeToBeats(NULL,pp,&num_measures,NULL,NULL,NULL)+ 0.000000000001;
+            double nbeats = floor(beats);
+            
+            beats -= nbeats;
+            
+            int fracbeats = (int) (1000.0 * beats);
+            
+            int *measptr = (int*)projectconfig_var_addr(NULL,__g_projectconfig_measoffs);
+            int nm=num_measures+1+(measptr ? *measptr : 0);
+            if (nm >= 100) bla[0]='0'+(nm/100)%10;//bars hund
+            if (nm >= 10) bla[1]='0'+(nm/10)%10;//barstens
+            bla[2]='0'+(nm)%10;//bars
+            
+            int nb=(int)nbeats+1;
+            if (nb >= 10) bla[3]='0'+(nb/10)%10;//beats tens
+            bla[4]='0'+(nb)%10;//beats
+            
+            
+            bla[7]='0' + (fracbeats/100)%10;
+            bla[8]='0' + (fracbeats/10)%10;
+            bla[9]='0' + (fracbeats%10); // frames
+        }
+        else
+        {
+            double *toptr = (double*)projectconfig_var_addr(NULL,__g_projectconfig_timeoffs);
+            if (toptr) pp+=(*toptr);
+            
+            int ipp=(int)pp;
+            int fr=(int)((pp-ipp)*1000.0);
+            
+            if (ipp >= 360000) bla[0]='0'+(ipp/360000)%10;//hours hundreds
+            if (ipp >= 36000) bla[1]='0'+(ipp/36000)%10;//hours tens
+            if (ipp >= 3600) bla[2]='0'+(ipp/3600)%10;//hours
+            
+            bla[3]='0'+(ipp/600)%6;//min tens
+            bla[4]='0'+(ipp/60)%10;//min
+            bla[5]='0'+(ipp/10)%6;//sec tens
+            bla[6]='0'+(ipp%10);//sec
+            bla[7]='0' + (fr/100)%10;
+            bla[8]='0' + (fr/10)%10;
+            bla[9]='0' + (fr%10); // frames
+        }
+        
+        if (m_mackie_lasttime_mode != tmode)
+        {
+            m_mackie_lasttime_mode=tmode;
+            SendMidiMessage(0x90, 0x71, tmode==5?0x7F:0); // set smpte light
+            SendMidiMessage(0x90, 0x72, m_mackie_lasttime_mode>0 && tmode<3?0x7F:0); // set beats light
+            
+        }
+        
+        if (memcmp(m_mackie_lasttime,bla,sizeof(bla)))
+        {
+            bool force=false;
+            if (now > m_mcu_timedisp_lastforce)
+            {
+                m_mcu_timedisp_lastforce=now+2000;
+                force=true;
+            }
+
+            for (int x =0 ; x < sizeof(bla) ; x ++)
+            {
+                int idx=sizeof(bla)-x-1;
+                if (bla[idx]!=m_mackie_lasttime[idx]||force)
+                {
+                    SendMidiMessage(0xB0,0x40+x,bla[idx]);
+                    m_mackie_lasttime[idx]=bla[idx];
+                }
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
 
 
 #endif /* control_surface_midi_widgets_h */
